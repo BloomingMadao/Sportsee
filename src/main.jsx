14267737e2397915cd28512cfc6ea22723ec71c4
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-09-15 12:50:22 +0200

    first commit - init of the project

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import Root from './routes/root';
+import ErrorPage from './error-page';
+// import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement:<ErrorPage />
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router ={router} />
+  </StrictMode>,
+)
