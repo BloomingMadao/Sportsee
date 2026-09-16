a5f8cb68b5c6689ffa1fc71c786532b2f2490b3d
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-09-16 16:26:37 +0200

    Add Recharts

--- src/pages/Connexion/index.jsx
+++ src/pages/Connexion/index.jsx
@@ -1,4 +1,7 @@
 import ConnexionForm from "../../components/ConnexionForm"
+//TODO: Faire le style du formulaire de connexion
+//Ajouter un components Image pour l'image a coté du formulaire de connexion
+
 function Connexion () {
     return(
         <>
